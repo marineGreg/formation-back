eaa2fe53c4a61399570bafe8567b9b2f0c571fbb
marineGreg <marineGreg@users.noreply.github.com>
2018-07-13 15:33:24 +0200

    index

--- php/boutique/index.php
+++ php/boutique/index.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/include/init.php';
 
 require __DIR__ . '/layout/top.php';
 ?>
-    <h1>Accueil</h1>
+    <h1>Page d'accueil</h1>
 <?php
 require __DIR__ . '/layout/bottom.php';
 ?>
